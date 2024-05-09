1402e12b8aa327095487cdd8fd5c2cbb17359819
Quenten <quentenwelch@gmail.com>
2024-05-09 12:58:21 -0600

    add update chore functionality

--- src/pages/ProfileChores/ProfileChores.jsx
+++ src/pages/ProfileChores/ProfileChores.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography, IconButton, Paper, Fab } from "@mui/material";
+import {
+  Box,
+  Typography,
+  IconButton,
+  Paper,
+  Fab,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
@@ -16,9 +24,8 @@ function ProfileChores() {
     balance: 50,
     lifetimeEarnings: 100,
   });
-  console.log(childId);
 
-  const navigate = useNavigate(); // Hook for navigation
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`http://localhost:8080/api/chores/child/${childId}`, {
@@ -43,14 +50,12 @@ function ProfileChores() {
     })
       .then((response) => {
         if (response.ok) {
-          // Update the local state to reflect the deletion
           const updatedChores = chores.filter(
             (chore) => chore.choreId !== choreId
           );
           setChores(updatedChores);
-          alert("Chore successfully deleted"); // use toast here? alert is kinda basic
+          alert("Chore successfully deleted");
         } else {
-          // Handle different server responses if the operation failed
           console.error(
             "Failed to delete chore, server responded with:",
             response.status
@@ -65,9 +70,53 @@ function ProfileChores() {
         alert("Failed to delete chore due to an error: " + error.message);
       });
   };
+
+  const updateChore = async (choreId, status) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/chores/update/${choreId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({ status }),
+        }
+      );
+      if (response.ok) {
+        const updatedChore = await response.json();
+        const updatedChores = chores.map((chore) =>
+          chore.choreId === choreId
+            ? { ...chore, status: updatedChore.status }
+            : chore
+        );
+        setChores(updatedChores);
+        alert("Chore status updated successfully");
+      } else {
+        console.error(
+          "Failed to update chore, server responded with:",
+          response.status
+        );
+        response
+          .text()
+          .then((text) => alert(`Failed to update chore: ${text}`));
+      }
+    } catch (error) {
+      console.error("Failed to update chore", error);
+      alert("Failed to update chore due to an error: " + error.message);
+    }
+  };
+
+  const handleStatusChange = (event, choreId) => {
+    const newStatus = event.target.value;
+    updateChore(choreId, newStatus);
+  };
+
   if (loading) {
     return <Typography>Loading...</Typography>;
   }
+
   const navigateToChoreCreation = () => {
     navigate(`/create-chore/${childId}`);
   };
@@ -107,18 +156,22 @@ function ProfileChores() {
                   <Typography variant="body2" color="text.secondary">
                     Due: {chore.dueDate.split("T")[0]}
                   </Typography>
-                  <Typography variant="body2" sx={{ color: chore.statusColor }}>
-                    {chore.status}
-                  </Typography>
                 </Box>
               </Box>
               <Box>
-                <IconButton
-                  onClick={() => console.log("Approval clicked")} // Replace with actual function
-                  color="success"
+                <Select
+                  value={chore.status}
+                  onChange={(event) => handleStatusChange(event, chore.choreId)}
+                  size="small"
+                  sx={{ ml: 1 }}
                 >
-                  <CheckCircleOutlineIcon />
-                </IconButton>
+                  <MenuItem value="COMPLETED">Completed</MenuItem>
+                  <MenuItem value="IN_PROGRESS">In Progress</MenuItem>
+                  <MenuItem value="AWAITING_APPROVAL">
+                    Awaiting Approval
+                  </MenuItem>
+                  <MenuItem value="NOT_ACCEPTED">Not Accepted</MenuItem>
+                </Select>
                 <IconButton
                   onClick={() => removeChore(chore.choreId)}
                   color="error"
